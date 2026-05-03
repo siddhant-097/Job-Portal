815f86f46b4533716026492c97518cb740f82a69
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-03 21:01:45 +0530

    feat: install Quill, added Dashboard, Add Job, Manage Jobs, View Applications page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,11 @@ import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
-
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
 
@@ -18,6 +22,11 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='add-job' element={<AddJob />}/>
+          <Route path='manage-jobs' element={<ManageJobs />}/>
+          <Route path='view-applications' element={<ViewApplications />}/>
+        </Route>
       </Routes>
     </div>
   )
