99839174a9d6ebd331f154b5d3310237c9cbab4f
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-06 23:18:04 +0530

    feat: , -> ; in routes

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
-app.get('/',(req,res)=> res.send("API Working")),
+app.get('/',(req,res)=> res.send("API Working"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
