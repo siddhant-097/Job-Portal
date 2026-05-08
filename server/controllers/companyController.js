36b1cbcc2a15fdc9f36b52f0bc1de428b5129ae1
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-08 14:23:58 +0530

    feat: added logic to register company and password encryption using bcrypt

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,40 +1,64 @@
+import Company from "../models/Company.js"
+import bcrypt from 'bcrypt'
 
 // Register a new company
-export const registerCompany = async (req,res) => {
-    
+export const registerCompany = async (req, res) => {
+
+    const { name, email, password } = req.body
+
+    const imageFile = req.file;
+
+    if (!name || !email || !password || !imageFile) {
+        return res.json({ success: false, message: "Missing Details" })
+    }
+
+    try {
+        const companyExists = await Company.findOne({ email })
+
+        if (companyExists) {
+            return res.json({ succes: false, message: 'Company already registered' })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+
+
+    } catch (error) {
+
+    }
 }
 
 // Company login
-export const loginCompany = async (req,res) => {
+export const loginCompany = async (req, res) => {
 
 }
 
 // Get company data
-export const getCompanyData = async (req,res) => {
+export const getCompanyData = async (req, res) => {
 
 }
 
 // Post a new job 
-export const postJob = async (req,res) => {
+export const postJob = async (req, res) => {
 
 }
 
 // Get Company Job Applicants
-export const getCompanyJobApplicants = async(req,res) => {
+export const getCompanyJobApplicants = async (req, res) => {
 
 }
 
 // Get Company Posted Jobs 
-export const getCompanyPostedJobs = async (req,res) => {
+export const getCompanyPostedJobs = async (req, res) => {
 
 }
 
 // Change Job Application Status
-export const ChangeJobApplicationsStatus = async (req,res) => {
+export const ChangeJobApplicationsStatus = async (req, res) => {
 
 }
 
 // Change Job Visibility
-export const changeVisibility = async (req,res) => {
+export const changeVisibility = async (req, res) => {
 
 }
