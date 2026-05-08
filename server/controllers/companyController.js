305e95c6ff4b980342215a71de146cda8e5dd718
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-08 16:57:50 +0530

    feat:added logic to upload company logo to cloudinary platform while registering a company

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,6 @@
 import Company from "../models/Company.js"
 import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary"
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -22,6 +23,9 @@ export const registerCompany = async (req, res) => {
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, salt)
 
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+       
 
     } catch (error) {
 
