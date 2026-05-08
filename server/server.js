3d135f97215f2986060abb49540a98b6bfe7b70e
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-08 23:14:40 +0530

    feat: add job API routes and getJobs controller

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 import companyRoutes from "./routes/companyRoutes.js"
 import connectCloudinary from "./config/cloudinary.js";
+import jobRoutes from './routes/jobRoutes.js'
 
 // Initialize Express
 const app = express()
@@ -26,6 +27,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 });
 app.post('/webhooks',clerkWebhooks)
 app.use('/api/company', companyRoutes)
+app.use('/api/jobs', jobRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000;
