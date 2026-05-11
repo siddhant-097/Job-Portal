1c41b91527bd3ca1dd1bd633487d14637be4dc15
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-11 13:19:00 +0530

    feat: add toast notifications and protect dashboard routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,22 +10,27 @@ import ViewApplications from './pages/ViewApplications'
 import Dashboard from './pages/Dashboard'
 import AddJob from './pages/AddJob'
 import 'quill/dist/quill.snow.css'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
 
-  const {showRecruiterLogin} = useContext(AppContext)
+  const {showRecruiterLogin, companyToken} = useContext(AppContext)
   
   return (
     <div>
       {showRecruiterLogin && <RecruiterLogin />}
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
         <Route path='/dashboard' element={<Dashboard />}>
+        {companyToken ? <>
           <Route path='add-job' element={<AddJob />}/>
           <Route path='manage-jobs' element={<ManageJobs />}/>
           <Route path='view-applications' element={<ViewApplications />}/>
+        </> : null
+        }
         </Route>
       </Routes>
     </div>
