11c3e3fb8ed8b10d519a384e1a753ff64a5c94ca
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-11 14:58:19 +0530

    feat: fetch job details from backend by id

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -9,6 +9,8 @@ import kconvert from 'k-convert'
 import moment from 'moment'
 import JobCard from '../components/JobCard'
 import Footer from '../components/Footer'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const ApplyJob = () => {
 
@@ -16,21 +18,29 @@ const ApplyJob = () => {
 
   const [JobData, setJobData] = useState(null)
 
-  const { jobs } = useContext(AppContext)
+  const { jobs, backendUrl } = useContext(AppContext)
 
   const fetchJob = async () => {
-    const data = jobs.filter(job => job._id === id)
-    if (data.length !== 0) {
-      setJobData(data[0])
-      console.log(data[0])
+
+    try {
+      
+      const {data} = await axios.get(backendUrl + `/api/jobs/${id}`)
+
+      if (data.success) {
+        setJobData(data.job)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
     }
+
   }
 
   useEffect(() => {
-    if (jobs.length > 0) {
-      fetchJob()
-    }
-  }, [id, jobs])
+    fetchJob()
+  }, [id])
 
   return JobData ? (
     <>
