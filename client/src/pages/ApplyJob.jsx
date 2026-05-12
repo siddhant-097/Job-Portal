dcae13b7153f8ba1f301b17fbcc511de4797e6e0
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-12 12:18:57 +0530

    feat: require login and resume before applying

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
@@ -16,9 +16,11 @@ const ApplyJob = () => {
 
   const { id } = useParams()
 
+  const navigate = useNavigate()
+
   const [JobData, setJobData] = useState(null)
 
-  const { jobs, backendUrl } = useContext(AppContext)
+  const { jobs, backendUrl, userData, userApplications } = useContext(AppContext)
 
   const fetchJob = async () => {
 
@@ -36,6 +38,23 @@ const ApplyJob = () => {
       toast.error(error.message)
     }
 
+  }
+
+  const applyHandler = async () => {
+    try {
+      
+      if (!userData) {
+        return toast.error('Login to apply for jobs')
+      }
+
+      if (!userData.resume) {
+        navigate('/applications')
+        return toast.error('Upload resume to apply')
+      }
+
+    } catch (error) {
+      
+    }
   }
 
   useEffect(() => {
@@ -74,7 +93,7 @@ const ApplyJob = () => {
             </div>
 
             <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
-              <button className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
+              <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
               <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
             </div>
 
@@ -84,7 +103,7 @@ const ApplyJob = () => {
             <div className='w-full lg:w-2/3'>
               <h2 className='font-bold text-2xl mb-4'>Job description</h2>
               <div className='rich-text' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
-               <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
+               <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
             </div>
             {/* Right Section - More Jobs */}
             <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
