5ad9c42eb0b0b09701644e2c7b78bc4177682c76
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-05-12 15:21:29 +0530

    feat: fetch and display company job applicants
    Add backend logic to retrieve applications for company-posted jobs, populate applicant and job details, and render real applicant data in the recruiter applications page.

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import { assets, viewApplicationsPageData } from '../assets/assets'
-
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
+import Loading  from '../components/Loading'
 const ViewApplications = () => {
-  return (
+
+    const {backendUrl, companyToken} = useContext(AppContext)
+
+    const [applicants, setApplicants] = useState(false)
+
+    // Funtion to fetch company Job Applications data
+    const fetchCompanyJobApplications = async () => {
+
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/company/applicants',
+                {headers: {token: companyToken}}
+            )
+
+            if (data.success) {
+                setApplicants(data.applications.reverse())
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        if (companyToken) {
+            fetchCompanyJobApplications()
+        }
+    }, [companyToken])
+
+  return applicants ? applicants.length === 0 ? ( <div></div> ) :  (
     <div className='container mx-auto p-4'>
         <div>
             <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'>
@@ -17,17 +55,17 @@ const ViewApplications = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {viewApplicationsPageData.map((applicant,index)=>(
+                    {applicants.filter( item => item.jobId && item.userId).map((applicant,index)=>(
                         <tr key={index} className='text-gray-700'>
                             <td className='py-2 px-4 border-b text-center'>{index+1}</td>
-                            <td className='py-2 px-4 border-b text-center flex'>
-                                <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.imgSrc} alt="" />
-                                <span>{applicant.name}</span>
+                            <td className='py-2 px-4 border-b text-center flex items-center'>
+                                <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.userId.image} alt="" />
+                                <span>{applicant.userId.name}</span>
                             </td>
-                            <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobTitle}</td>
-                            <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.location}</td>
+                            <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobId.title}</td>
+                            <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobId.location}</td>
                             <td className='py-2 px-4 border-b'>
-                                <a href="" target='_blank'
+                                <a href={applicant.userId.resume} target='_blank'
                                     className='bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center'
                                 >
                                     Resume <img src={assets.resume_download_icon} alt="" />
@@ -48,7 +86,7 @@ const ViewApplications = () => {
             </table>
         </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default ViewApplications
--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -127,7 +127,21 @@ export const postJob = async (req, res) => {
 
 // Get Company Job Applicants
 export const getCompanyJobApplicants = async (req, res) => {
+    try {
+        
+        const companyId = req.company._id
 
+        // Find job applications for the user and populate related data
+        const applications = await JobApplication.find({companyId})
+        .populate('userId', 'name image resume')
+        .populate('jobId', 'title location category level salary')
+        .exec()
+
+        return res.json({success: true, applications})
+
+    } catch (error) {
+        res.json({success:false, messafe:error.message})
+    }
 }
 
 // Get Company Posted Jobs 
